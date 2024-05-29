85428c31936c20a738411c0afaa5017004d6e7ba
vuduckhoa <khoavu27082k4@gmail.com>
2024-05-29 20:15:28 +0700

    lesson6

---
+++ Vdkesson06/lesson06/lesson06/Controllers/VdkSongController.cs
@@ -0,0 +1,29 @@
+﻿using lesson06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lesson06.Controllers
+{
+    public class VdkSongController : Controller
+    {
+        private static List<VdkSong> pvvSongs = new List<VdkSong>()
+        {
+          new VdkSong{ Id = 221090032, VdkTitle = "Vu Duc Khoa", VdkAuthor = "k22cntt3", VdkArtist = "NTU", VdkYearRelease="2020"},
+          new VdkSong{ Id = 1, VdkTitle = "Vu Duc khoa", VdkAuthor = "k22cnt3", VdkArtist = "NTU", VdkYearRelease="2020"}
+        };
+
+        // GET: VdkSong
+        public ActionResult VdkIndex()
+        {
+            return View(VdkSongs);
+        }
+        public ActionResult VdkCreate() 
+        {
+            var Vdksong = new VdkSong();
+            return View(Vdksong);
+        }
+    }
+}
---
+++ Vdkesson06/lesson06/lesson06/Models/VdkSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lesson06.Models
+{
+    public class VdkSong
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Vdk:hay nhap tieu de")]
+        [DisplayName("tieu de")]
+        public string VdkTitle { get; set; }
+        [Required(ErrorMessage ="Vdk:hay nhap tac gia")]
+        [DisplayName("tac gia")]
+
+        public string VdkAuthor { get; set; }
+        [Required(ErrorMessage ="Vdk:hay nhap nghe si")]
+        [StringLength(50,MinimumLength =2, ErrorMessage = "Vdk:ten nghe si toi thieu 2 ki tu,toi da 50 ky tu")]
+        [DisplayName("nghe si")]
+
+        public string VdkArtist { get; set; }
+        [Required(ErrorMessage ="Vdk:hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Vdk:nhap xuat ban phai co 2 ky tu so ")]
+        [Range(1900,2024, ErrorMessage = "Vdk:nam xuat ban khoang 1900-2024")]
+        public string VdkYearRelease { get; set; }
+    }
+}
